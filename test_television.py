437447dab3c193734cd1a2efebacf2d777eb2283
Suheb Abdi <suheb@Suhebs-MacBook-Air.local>
2026-04-10 23:30:25 -0500

    Final test file

--- test_television.py
+++ test_television.py
@@ -1,5 +1,45 @@
 from television import Television
 
-def test_init():
-    tv = Television()
-    assert str(tv) == "Power = False, Channel = 0, Volume = 0"
+class TestTelevision:
+
+    def setup_method(self):
+        self.tv = Television()
+
+    def teardown_method(self):
+        del self.tv
+
+    def test_init(self):
+        assert self.tv.__str__() == "Power = False, Channel = 0, Volume = 0"
+
+    def test_power(self):
+        self.tv.power()
+        assert self.tv.__str__() == "Power = True, Channel = 0, Volume = 0"
+        self.tv.power()
+        assert self.tv.__str__() == "Power = False, Channel = 0, Volume = 0"
+
+    def test_mute(self):
+        self.tv.power()
+        self.tv.volume_up()
+        self.tv.mute()
+        assert self.tv.__str__() == "Power = True, Channel = 0, Volume = 0"
+
+    def test_channel_up(self):
+        self.tv.power()
+        self.tv.channel_up()
+        assert self.tv.__str__() == "Power = True, Channel = 1, Volume = 0"
+
+    def test_channel_down(self):
+        self.tv.power()
+        self.tv.channel_down()
+        assert self.tv.__str__() == "Power = True, Channel = 3, Volume = 0"
+
+    def test_volume_up(self):
+        self.tv.power()
+        self.tv.volume_up()
+        assert self.tv.__str__() == "Power = True, Channel = 0, Volume = 1"
+
+    def test_volume_down(self):
+        self.tv.power()
+        self.tv.volume_up()
+        self.tv.volume_down()
+        assert self.tv.__str__() == "Power = True, Channel = 0, Volume = 0"
